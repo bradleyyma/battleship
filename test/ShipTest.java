fc8de53fbc140b483d55154693d6bab4b53616d0
Bradley <bradley.ma@colorado.edu>
2021-02-15 16:22:46 -0700

    created test cases for Ship size, hit, and sunk

--- test/ShipTest.java
+++ test/ShipTest.java
@@ -16,5 +16,28 @@ public class ShipTest{
         assertEquals(name, "Battleship");
     }
 
+    @Test
+    public void testSizeOfShip(){
+        Ship ship1 = new Ship();
+        ship1.setSize(2);
+        int size = ship1.getSize();
+        assertEquals(size, 2);
+    }
+
+    @Test
+    public void testHit(){
+        Ship ship1 = new Ship();
+        ship1.hit();
+        assertEquals(1, ship1.getNumHits());
+    }
+
+    public void testSunk(){
+        Ship ship1 = new Ship();
+        ship1.setSize(2);
+        ship1.hit();
+        ship1.hit();
+        assertEquals(true, ship1.sunkStatus())
+    }
+
 
 }
