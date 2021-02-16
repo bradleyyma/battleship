e470588509027f102736e6816222307f86361633
Maksim Vasilyev <49633171+mava98@users.noreply.github.com>
2021-02-15 17:19:51 -0700

    First tests for Grid class

--- src/edu/colorado/caterpillars/Grid.java
+++ src/edu/colorado/caterpillars/Grid.java
@@ -1,4 +1,14 @@
 package edu.colorado.caterpillars;
 
 public class Grid {
+
+    private int[][] gridP1Upper = new int[10][10];
+    private int[][] gridP1Lower = new int[10][10];
+    private int[][] gridP2Upper = new int[10][10];
+    private int[][] gridP2Lower = new int[10][10];
+
+    public setGrid(){
+
+    }
+
 }
--- test/GridTest.java
+++ test/GridTest.java
@@ -3,4 +3,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GridTest {
+    @Test
+    public void testInititializeGrid() {
+        Grid grid1 = new Grid();
+        int[][] gridTest = new int[10][10];
+        assertEquals(gridTest, grid1.getGrid("gridP1Upper"));
+    }
 }
