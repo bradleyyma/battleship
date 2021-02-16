c104107de0e45841cd3850e6ef77bb140498fe0b
Maksim Vasilyev <49633171+mava98@users.noreply.github.com>
2021-02-15 17:50:17 -0700

    Grid test to see if ship objects can be added to grid

--- src/edu/colorado/caterpillars/Grid.java
+++ src/edu/colorado/caterpillars/Grid.java
@@ -7,8 +7,18 @@ public class Grid {
     private int[][] gridP2Upper = new int[10][10];
     private int[][] gridP2Lower = new int[10][10];
 
-    public setGrid(){
-
+    public int[][] getGrid(String name){
+        switch(name){
+            case "gridP1Upper":
+                return gridP1Upper;
+            case "gridP2Upper":
+                return gridP2Upper;
+            case "gridP1Lower":
+                return gridP1Lower;
+            case "gridP2Lower":
+                return gridP2Lower;
+        }
+        return new int[0][0];
     }
 
 }
--- test/GridTest.java
+++ test/GridTest.java
@@ -1,12 +1,24 @@
 import edu.colorado.caterpillars.Grid;
+import edu.colorado.caterpillars.Ship;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GridTest {
     @Test
-    public void testInititializeGrid() {
+    public void testGrid(){
         Grid grid1 = new Grid();
         int[][] gridTest = new int[10][10];
-        assertEquals(gridTest, grid1.getGrid("gridP1Upper"));
+        assertArrayEquals(gridTest, grid1.getGrid("gridP1Upper"));
+    }
+    @Test
+    public void testShipInGrid(){
+        Grid grid1 = new Grid();
+        int[][] gridTest = new int[10][10];
+        gridTest[2][4] = 2;
+        gridTest[2][5] = 2;
+        Ship ship1 = new Ship();
+        ship1.setSize(2);
+        grid1.addShip(2,ship1,2,4,"S");
+        assertArrayEquals(gridTest, grid1.getGrid("gridP2Lower"));
     }
 }
