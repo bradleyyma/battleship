e3a772ef217b38f10edb63c6bcc23f6371b73803
Bradley <bradley.ma@colorado.edu>
2021-02-18 17:21:04 -0700

    Added / Updated GridTests to check for hits/sunks

--- test/GridTest.java
+++ test/GridTest.java
@@ -17,14 +17,27 @@ public class GridTest {
         gridTest[2][4] = 1;
         gridTest[2][5] = 1;
         Ship ship1 = new Ship(1, "Battleship", 2);
-        grid1.addShip(2, ship1 ,2,4,"W");
+        grid1.addShip(2, ship1 ,2,4,"E");
         assertArrayEquals(gridTest, grid1.getGrid("gridP2Lower"));
     }
     @Test
     public void testHitOrMiss(){
         Grid grid1 = new Grid();
-        String hitOrMiss = grid1.hitOrMiss(2,4,2);
-        assertEquals("MISS",hitOrMiss);
+        Ship ship = new Ship(2, "Sweeps", 2);
+        grid1.addShip(1, ship, 0, 0, "E");
+        grid1.addShip(2, ship, 1, 0, "S");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(i == 0 && j == 0 || i == 0 && j == 1) // where the ship is in player1's grid
+                    assertNotEquals("MISS",grid1.hitOrMiss(i,j,1));
+                else if(j == 0 && i == 1 || j == 0 && i == 2) // where the ship is in player2's grid
+                    assertNotEquals("MISS",grid1.hitOrMiss(i,j,2));
+                else { // anything else should MISS
+                    assertEquals("MISS", grid1.hitOrMiss(i, j, 1));
+                    assertEquals("MISS", grid1.hitOrMiss(i, j, 2));
+                }
+            }
+        }
     }
     @Test
     public void testDisplayOpponentFleet(){
@@ -38,4 +51,15 @@ public class GridTest {
         int[][] gridTest = new int[10][10];
         assertArrayEquals(gridTest, grid1.displayPlayerFleet(1));
     }
+
+    @Test
+    public void testSunk(){
+        Grid grid = new Grid();
+        Ship ship = new Ship(2, "Minesweeper", 2);
+        grid.addShip(1, ship, 0, 0, "E");
+        assertEquals("HIT", grid.hitOrMiss(0,0, 1));
+        assertEquals("SUNK Minesweeper", grid.hitOrMiss(0,1, 1));
+    }
+
+
 }
