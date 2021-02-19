087e5475eb78a3ebd995e514ae288441091579c3
Bradley <bradley.ma@colorado.edu>
2021-02-18 17:49:20 -0700

    Refactored repeated code into @BeforeEach, check for thrown exception in getGrid

--- test/GridTest.java
+++ test/GridTest.java
@@ -9,59 +9,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GridTest {
 
+    private Grid grid;
+
     @BeforeEach
-    public void createGridForEachPlayer() {
-        Grid gridP1Upper = new Grid();
-        Grid gridP2Upper = new Grid();
-        Grid gridP1Lower = new Grid();
-        Grid gridP2Lower = new Grid();
+    public void createGrid() {
+        grid = new Grid();
     }
 
     @Test
     public void testGrid(){
-        Grid grid1 = new Grid();
         int[][] gridTest = new int[10][10];
-        assertArrayEquals(gridTest, grid1.getGrid("gridP1Upper"));
+        assertArrayEquals(gridTest, grid.getGrid("gridP1Upper"));
+        assertArrayEquals(gridTest, grid.getGrid("gridP1Lower"));
+        assertArrayEquals(gridTest, grid.getGrid("gridP2Upper"));
+        assertArrayEquals(gridTest, grid.getGrid("gridP2Lower"));
+        assertThrows(Exception.class, () -> grid.getGrid("randomName"));
     }
+
     @Test
     public void testShipInGrid(){
-        Grid grid1 = new Grid();
         int[][] gridTest = new int[10][10];
         gridTest[2][4] = 1;
         gridTest[2][5] = 1;
         Ship ship1 = new Ship(1, "Battleship", 2);
-        grid1.addShip(2, ship1 ,2,4,"E");
-        assertArrayEquals(gridTest, grid1.getGrid("gridP2Lower"));
+        grid.addShip(2, ship1 ,2,4,"E");
+        assertArrayEquals(gridTest, grid.getGrid("gridP2Lower"));
     }
+
     @Test
     public void testAttack(){
-        Grid grid1 = new Grid();
         Ship ship = new Ship(2, "Sweeps", 2);
-        grid1.addShip(1, ship, 0, 0, "E");
-        grid1.addShip(2, ship, 1, 0, "S");
+        grid.addShip(1, ship, 0, 0, "E");
+        grid.addShip(2, ship, 1, 0, "S");
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
                 if(i == 0 && j == 0 || i == 0 && j == 1) // where the ship is in player1's grid
-                    assertNotEquals("MISS",grid1.attack(i,j,1));
+                    assertNotEquals("MISS", grid.attack(i, j,1));
                 else if(j == 0 && i == 1 || j == 0 && i == 2) // where the ship is in player2's grid
-                    assertNotEquals("MISS",grid1.attack(i,j,2));
+                    assertNotEquals("MISS", grid.attack(i, j,2));
                 else { // anything else should MISS
-                    assertEquals("MISS", grid1.attack(i, j, 1));
-                    assertEquals("MISS", grid1.attack(i, j, 2));
+                    assertEquals("MISS", grid.attack(i, j, 1));
+                    assertEquals("MISS", grid.attack(i, j, 2));
                 }
             }
         }
     }
+
     @Test
     public void testDisplayGrid(){
-        Grid grid1 = new Grid();
         int[][] gridTest = new int[10][10];
-        assertArrayEquals(gridTest, grid1.getGrid("gridP2Upper"));
+        assertArrayEquals(gridTest, grid.getGrid("gridP2Upper"));
     }
     
     @Test
     public void testSunk(){
-        Grid grid = new Grid();
         Ship ship = new Ship(2, "Minesweeper", 2);
         grid.addShip(1, ship, 0, 0, "E");
         assertEquals("HIT", grid.attack(0,0, 1));
