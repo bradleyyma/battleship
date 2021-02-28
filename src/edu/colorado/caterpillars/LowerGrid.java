92c883aead4464898251d9b2a34679891668c155
Bradley <bradley.ma@colorado.edu>
2021-02-28 16:58:48 -0700

    CODE: addShip() function with error check private method

--- src/edu/colorado/caterpillars/LowerGrid.java
+++ src/edu/colorado/caterpillars/LowerGrid.java
@@ -2,8 +2,8 @@ package edu.colorado.caterpillars;
 
 public class LowerGrid extends Grid{
     public void addShip(Ship ship, int row, int col, String direction){
-//        if(doesOverlap(ship, row, col, direction))
-//            throw new IllegalArgumentException("Ships cannot overlap!");
+        if(isInvalidPlacement(ship, row, col, direction)) // checks for overlap, out of bounds, and invalid direction
+            throw new IllegalArgumentException("Ships cannot overlap!");
 
         for(int i = 0; i < ship.getSize(); i++){
             if (direction == "N"){
@@ -18,35 +18,31 @@ public class LowerGrid extends Grid{
             else if (direction == "W"){
                 grid[row][col--] = ship.id;
             }
-            else{
-                System.out.println("addShip is broken");
-            }
         }
     }
-//
-//    private boolean doesOverlap(int [][] grid,Ship ship,int row,int col, String direction){
-//        for(int i = 0; i < ship.getSize(); i++){
-//            if (direction == "N"){
-//                if(grid[row--][col] != 0)
-//                    return true;
-//            }
-//            else if (direction == "S"){ // South means increasing row num (e.g A1 then A2)
-//                if(grid[row++][col] != 0)
-//                    return true;
-//            }
-//            else if (direction == "E"){
-//                if(grid[row][col++] != 0)
-//                    return true;
-//            }
-//            else if (direction == "W"){
-//                if(grid[row][col--] != 0)
-//                    return true;
-//            }
-//            else{
-//                // TODO: throw exception for invalid direction
-//                return false;
-//            }
-//        }
-//        return false;
-//    }
+
+    private boolean isInvalidPlacement(Ship ship, int row, int col, String direction){
+        for(int i = 0; i < ship.getSize(); i++){
+            if (direction == "N"){
+                if(grid[row--][col] != 0)
+                    return false;
+            }
+            else if (direction == "S"){ // South means increasing row num (e.g A1 then A2)
+                if(grid[row++][col] != 0)
+                    return true;
+            }
+            else if (direction == "E"){
+                if(grid[row][col++] != 0)
+                    return true;
+            }
+            else if (direction == "W"){
+                if(grid[row][col--] != 0)
+                    return true;
+            }
+            else{
+                throw new IllegalArgumentException("Not a valid direction!");
+            }
+        }
+        return false;
+    }
 }
