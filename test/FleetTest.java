dc38768e020aa6f79b94325cdb76e9dafc674fb2
Bradley <bradley.ma@colorado.edu>
2021-03-01 23:08:42 -0700

    TEST: updated tests to use new Ship subclasses, also test for correct ordering of new ids and cids of ships in fleet

--- test/FleetTest.java
+++ test/FleetTest.java
@@ -1,6 +1,4 @@
-import edu.colorado.caterpillars.Fleet;
-import edu.colorado.caterpillars.Grid;
-import edu.colorado.caterpillars.Ship;
+import edu.colorado.caterpillars.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +14,9 @@ public class FleetTest {
     @BeforeEach
     public void createFleet(){
         fleet = new Fleet();
-        ship1 = new Ship(4, "Battleship", 4);
-        ship2 = new Ship(2, "Minesweeper", 2);
-        ship3 = new Ship(3, "Destroyer", 3);
+        ship1 = new Battleship();
+        ship2 = new Minesweeper();
+        ship3 = new Destroyer();
         fleet.addShip(ship1);
         fleet.addShip(ship2);
         fleet.addShip(ship3);
@@ -26,8 +24,12 @@ public class FleetTest {
 
     @Test
     public void testGetShipById(){
+        Ship fleetShipId2 = fleet.getShipById(2);
+        assertSame(ship1,fleetShipId2); // ship1 should get id = 2 and cid = 3
         Ship fleetShipId4 = fleet.getShipById(4);
-        assertSame(ship1,fleetShipId4);
+        assertSame(ship2,fleetShipId4); // ship2 should get id = 4 and cid = 5
+        Ship fleetShipId6 = fleet.getShipById(6);
+        assertSame(ship3,fleetShipId6); // ship3 should get id = 6 and cid = 7
     }
 
     @Test
@@ -44,19 +46,19 @@ public class FleetTest {
     public void testSinkThreeShipFleet(){
         assertEquals(3,fleet.getNumSurvivingShips());
 
-        Ship fleetShipId4 = fleet.getShipById(4);
+        Ship fleetShipId4 = fleet.getShipById(2); // Battleship ( size 4 )
         fleetShipId4.hit();
         fleetShipId4.hit();
         fleetShipId4.hit();
         fleetShipId4.hit();
         assertEquals(2,fleet.getNumSurvivingShips());
 
-        Ship fleetShipId2 = fleet.getShipById(2);
+        Ship fleetShipId2 = fleet.getShipById(4); //Minesweeper ( size 2 )
         fleetShipId2.hit();
         fleetShipId2.hit();
         assertEquals(1,fleet.getNumSurvivingShips());
 
-        Ship fleetShipId3 = fleet.getShipById(3);
+        Ship fleetShipId3 = fleet.getShipById(6); //Destroyer ( size 3 )
         fleetShipId3.hit();
         fleetShipId3.hit();
         fleetShipId3.hit();
@@ -67,19 +69,19 @@ public class FleetTest {
     public void testSinkFleetHitById(){
         assertEquals(3,fleet.getNumSurvivingShips());
 
-        fleet.hitShipById(4);
-        fleet.hitShipById(4);
-        fleet.hitShipById(4);
-        fleet.hitShipById(4);
+        fleet.hitShipById(2);
+        fleet.hitShipById(2);
+        fleet.hitShipById(2);
+        fleet.hitShipById(2);
         assertEquals(2,fleet.getNumSurvivingShips());
 
-        fleet.hitShipById(2);
-        fleet.hitShipById(2);
+        fleet.hitShipById(4);
+        fleet.hitShipById(4);
         assertEquals(1,fleet.getNumSurvivingShips());
 
-        fleet.hitShipById(3);
-        fleet.hitShipById(3);
-        fleet.hitShipById(3);
+        fleet.hitShipById(6);
+        fleet.hitShipById(6);
+        fleet.hitShipById(6);
         assertEquals(0,fleet.getNumSurvivingShips());
     }
 }
