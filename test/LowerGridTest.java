43adcdf1ceee75f8c69a26d2519718d0af5f96ae
Bradley <bradley.ma@colorado.edu>
2021-03-01 23:28:28 -0700

    TEST: Updated LowerGridTests to use new subclasses, adjusted overlap ships to check valid add after invalid add

--- test/LowerGridTest.java
+++ test/LowerGridTest.java
@@ -14,32 +14,34 @@ public class LowerGridTest {
     @Test
     public void testAddShip(){
         int[][] gridTest = new int[10][10];
-        int id1 = 2;
-        int id2 = 3;
-        Ship ship1 = new Ship(id1, "Minesweeper", 2);
-        Ship ship2 = new Ship(id2, "Destroyer", 3);
+        Ship ship1 = new Minesweeper();
+        Ship ship2 = new Destroyer();
         grid.addShip(ship1 ,2,4,"E");
         grid.addShip(ship2, 9, 9, "N");
-        gridTest[2][4] = gridTest[2][5] = id1;
-        gridTest[9][9] = gridTest[8][9] = gridTest[7][9] = id2;
+        gridTest[2][4] = gridTest[2][5] = 2; // id of first ship added
+        gridTest[9][9] = gridTest[8][9] = gridTest[7][9] = 4; //id of second ship added
         assertArrayEquals(gridTest, grid.getGrid());
     }
 
     @Test
     public void testOutOfBoundsShip(){
-        Ship ship1 = new Ship(2, "Destroyer", 3);
+        Ship ship1 = new Destroyer();
         assertThrows(Exception.class, () -> grid.addShip(ship1, 1, 0, "N"));
         assertThrows(Exception.class, () -> grid.addShip(ship1, -1, 1, "N")); //out of bounds to start
         assertThrows(Exception.class, () -> grid.addShip(ship1, 0, 1, "X")); // invalid direction
         // Also check that the grid is correct (no partial ships were added)
         int[][] gridTest = new int[10][10];
         assertArrayEquals(gridTest, grid.getGrid());
+        // Now check we can re add the ship(validly) correctly
+        grid.addShip(ship1, 0, 0, "E");
+        gridTest[0][0] = gridTest[0][1] = gridTest[0][2] = 2;
+        assertArrayEquals(gridTest, grid.getGrid());
     }
 
     @Test
     public void testOverlappingShip(){
-        Ship ship1 = new Ship(2, "Minesweeper", 2);
-        Ship ship2 = new Ship(3, "Destroyer", 3);
+        Ship ship1 = new Minesweeper();
+        Ship ship2 = new Destroyer();
         grid.addShip(ship1, 0, 0, "S");
         assertThrows(Exception.class, () -> grid.addShip(ship2, 0, 0, "E"));
         // Also check that the grid is correct (no partial ship2 were added)
@@ -51,7 +53,7 @@ public class LowerGridTest {
 
     @Test
     public void testAttack(){
-        Ship ship = new Ship(2, "Sweeps", 2);
+        Ship ship = new Minesweeper();
         grid.addShip(ship, 0, 0, "E");
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
@@ -65,8 +67,8 @@ public class LowerGridTest {
 
     @Test
     public void testSunkAndSurrender(){
-        Ship ship = new Ship(2, "Minesweeper", 2);
-        Ship ship2 = new Ship(3, "Destroyer", 3);
+        Ship ship = new Minesweeper();
+        Ship ship2 = new Destroyer();
         grid.addShip(ship, 0, 0, "E");
         grid.addShip(ship2, 1, 0, "E");
         assertEquals("HIT", grid.receiveAttack(0,0));
@@ -80,7 +82,7 @@ public class LowerGridTest {
     public void testLowerGridValues(){
         int [][] testGrid = new int[10][10];
         int shipId = 2;
-        Ship ship1 = new Ship(shipId, "Minesweeper", 2);
+        Ship ship1 = new Minesweeper();
         grid.addShip(ship1, 0, 0, "E"); //ship is at (0, 0) and (0, 1)
         testGrid[0][0] = testGrid[0][1] = shipId;
         assertArrayEquals(testGrid, grid.getGrid());
