1199e92d3c86a1ba1cbb51f67d5ea2299029da00
Maksim Vasilyev <49633171+mava98@users.noreply.github.com>
2021-03-03 16:52:52 -0700

    Added a test to SonarPulse that tests weather the SonarPulse accurately displays the information with ships involved.

--- test/SonarPulseTest.java
+++ test/SonarPulseTest.java
@@ -32,6 +32,22 @@ public class SonarPulseTest {
     }
     @Test
     public void testFireWithShip(){
-
+        Ship ship1 = new Battleship();
+        Ship ship2 = new Battleship();
+        grid.addShip(ship1, 4, 5, "E"); //Ship is at (4,5), (4,6), and (4,7);
+        grid.addShip(ship2, 6, 4, "S"); //Ship is at (6,4), (7,4), and (8,4);
+        int [][] testGrid = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, -1, 0, 0, 0, 0},
+                {0, 0, 0, 0, -1, 1, 1, 0, 0, 0},
+                {0, 0, 0, -1, -1, -1, -1, -1, 0, 0},
+                {0, 0, 0, 0, 1, -1, -1, 0, 0, 0},
+                {0, 0, 0, 0, 0, -1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
+        assertArrayEquals(testGrid, pulse.fire(5,5));
     }
 }
