f60218d19e71e1d18f9f93ba7b36bd919b1b1b66
Bradley <bradley.ma@colorado.edu>
2021-03-10 11:23:32 -0700

    CODE: added default constructor for Player Class and addShip method with getter for lower grid

--- src/edu/colorado/caterpillars/Player.java
+++ src/edu/colorado/caterpillars/Player.java
@@ -1,4 +1,22 @@
 package edu.colorado.caterpillars;
 
 public class Player {
+    private Player opponent;
+    private LowerGrid lower;
+
+    public Player(){
+        lower = new LowerGrid();
+    }
+
+    public int [][] getLowerGrid() {
+        return lower.getGrid();
+    }
+
+    public void setOpponent(Player opponent){
+        this.opponent = opponent;
+    }
+
+    public void addShip(Ship ship, int row, int col, String dir){
+        lower.addShip(ship, row, col, dir);
+    }
 }
