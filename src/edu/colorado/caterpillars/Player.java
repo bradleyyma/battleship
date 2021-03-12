2c24a333dcb891332d58d2cddb349fec99e10ab2
Bradley <bradley.ma@colorado.edu>
2021-03-11 21:44:59 -0700

    CODE: Implementing player.attack() and adding some getters needed

--- src/edu/colorado/caterpillars/Player.java
+++ src/edu/colorado/caterpillars/Player.java
@@ -3,20 +3,30 @@ package edu.colorado.caterpillars;
 public class Player {
     private Player opponent;
     private LowerGrid lower;
+    private UpperGrid upper;
 
     public Player(){
         lower = new LowerGrid();
     }
 
-    public int [][] getLowerGrid() {
-        return lower.getGrid();
+    public LowerGrid getLower(){
+        return lower;
+    }
+
+    public UpperGrid getUpper(){
+        return upper;
     }
 
     public void setOpponent(Player opponent){
         this.opponent = opponent;
+        upper = new UpperGrid(this.opponent.getLower());
     }
 
     public void addShip(Ship ship, int row, int col, String dir){
         lower.addShip(ship, row, col, dir);
+    }
+
+    public String attack(int row, int col){
+        return upper.sendAttack(row, col);
     }
 }
