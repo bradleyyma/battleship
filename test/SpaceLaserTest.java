7f7bf3c59da1ad642c1af7c7ab21c98bead1f517
Bradley <bradley.ma@colorado.edu>
2021-03-16 14:33:34 -0600

    TEST: SpaceLaser usage unit tests

--- test/SpaceLaserTest.java
+++ test/SpaceLaserTest.java
@@ -1,2 +1,24 @@
+import edu.colorado.caterpillars.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SpaceLaserTest {
+
+    private SpaceLaser laser;
+    @BeforeEach
+    public void createObjects(){
+        LowerGrid lower = new LowerGrid();
+        Ship bat = new Battleship();
+        Ship sub = new Submarine();
+        lower.addShip(bat, 0, 0, "E", false);
+        lower.addShip(sub, 1, 4, "W", true);
+        laser = new SpaceLaser(lower);
+    }
+
+    @Test
+    public void testAttackMissSurfaceHitSub(){
+        assertEquals("HIT", laser.use(1, 2));
+    }
 }
