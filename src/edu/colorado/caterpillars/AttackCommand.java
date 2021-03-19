c4f8b2d25c7cb472c0c656fd66bf0073748da8e1
Bradley <bradley.ma@colorado.edu>
2021-03-18 18:40:49 -0600

    CODE: Initial implmentation of command pattern

--- src/edu/colorado/caterpillars/AttackCommand.java
+++ src/edu/colorado/caterpillars/AttackCommand.java
@@ -1,2 +1,25 @@
-package edu.colorado.caterpillars;public class AttackCommand {
+package edu.colorado.caterpillars;
+
+public class AttackCommand implements Command{
+    private int row;
+    private int col;
+    private Player player;
+
+
+    public AttackCommand(Player player, int row, int col){
+        this.row = row;
+        this.col = col;
+        this.player = player;
+    }
+    @Override
+    public void execute() {
+        player.attack(row, col);
+    }
+
+
+
+    @Override
+    public void undo() {
+        // TODO:
+    }
 }
--- src/edu/colorado/caterpillars/MoveEastCommand.java
+++ src/edu/colorado/caterpillars/MoveEastCommand.java
@@ -1,2 +1,18 @@
-package edu.colorado.caterpillars;public class MoveEastCommand {
-}
+package edu.colorado.caterpillars;
+
+//public class MoveEastCommand implements Command{
+//
+//    private LowerGrid lower;
+//    public MoveEastCommand(LowerGrid lower){
+//        this.lower = lower;
+//    }
+//    @Override
+//    public void execute() {
+//        lower.moveFleet("E");
+//    }
+//
+//    @Override
+//    public void undo() {
+//        lower.moveFleet("W");
+//    }
+//}
