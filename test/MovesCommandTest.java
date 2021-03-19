d38d504625222a347d32da2ff7d4deef160e9c5c
JazKarit <jaskrits.singh@gmail.com>
2021-03-18 19:15:43 -0600

    Test Move East and undo, Passed

--- test/MovesCommandTest.java
+++ test/MovesCommandTest.java
@@ -1,8 +1,34 @@
+import edu.colorado.caterpillars.*;
 import org.junit.jupiter.api.Test;
-//
-//public class MovesCommandTest {
-//    @Test
-//    public void testMoveEast(){
-//        MoveEastCommand command = new MoveEastCommand();
-//    }
-//}
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MovesCommandTest {
+    @Test
+    public void testMoveEastAndUndo(){
+        Player player = new Player();
+        Ship ship = new Destroyer();
+        String dir = "E";
+        player.addShip(ship,1,1,dir,false);
+        MoveFleetCommand moveEastCommand = new MoveFleetCommand(player,dir);
+        int[][] gridTest = new int[10][10];
+        gridTest[1][1] = 2;
+        gridTest[1][2] = 3;
+        gridTest[1][3] = 2;
+        assertArrayEquals(gridTest, player.getLower().getGrid());
+
+        moveEastCommand.execute();
+        gridTest[1][1] = 0;
+        gridTest[1][2] = 2;
+        gridTest[1][3] = 3;
+        gridTest[1][4] = 2;
+        assertArrayEquals(gridTest, player.getLower().getGrid());
+
+        moveEastCommand.undo();
+        gridTest[1][1] = 2;
+        gridTest[1][2] = 3;
+        gridTest[1][3] = 2;
+        gridTest[1][4] = 0;
+        assertArrayEquals(gridTest, player.getLower().getGrid());
+    }
+}
