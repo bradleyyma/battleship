5b3fbd1fdc3ace3ae1f1842cf25c1de963caa2df
Bradley <bradley.y.ma@gmail.com>
2021-04-02 16:55:29 -0600

    TEST: Updated Bomb tests for requirments and usage limits

--- app/src/test/java/edu/colorado/caterpillars/BombTest.java
+++ app/src/test/java/edu/colorado/caterpillars/BombTest.java
@@ -7,21 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BombTest {
     Bomb bomb;
     LowerGrid lower;
-    int [][] testGrid;
+    UpperGrid upper;
+    int [][] testLowerGrid;
+    int [][] testUpperGrid;
+    private SunkData sunkData;
     @BeforeEach
     public void createObjects(){
         lower = new LowerGrid();
-        bomb = new Bomb(lower);
+        sunkData = new SunkData(lower.getFleet());
+
+        upper = new UpperGrid(lower, sunkData);
+        bomb = new Bomb(lower, upper, sunkData);
         Ship bat = new Battleship();
         Ship des = new Destroyer();
         lower.addShip(bat, 0, 0, "E", false); //at (0, 0), (0, 1), (0, 2), (0, 3) on surface
         lower.addShip(des, 1, 0, "E", false);  //at (1, 0), (1, 1), (1, 2)
-        testGrid = new int [10][10];
-        testGrid[0][0] = testGrid[0][1] = testGrid[0][3] = 2;
-        testGrid[0][2] = 3;
-        testGrid[1][0] = testGrid[1][2] = 4;
-        testGrid[1][1] = 5;
-        //int [][] testGrid = {
+        testLowerGrid = new int [10][10];
+        testLowerGrid[0][0] = testLowerGrid[0][1] = testLowerGrid[0][3] = 2;
+        testLowerGrid[0][2] = 3;
+        testLowerGrid[1][0] = testLowerGrid[1][2] = 4;
+        testLowerGrid[1][1] = 5;
+        //int [][] testLowerGrid = {
         //                {2, 2, 3, 2, 0, 0, 0, 0, 0, 0},
         //                {4, 5, 4, 0, 0, 0, 0, 0, 0, 0},
         //                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -33,37 +39,47 @@ public class BombTest {
         //                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         //                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         //        };
+        testUpperGrid = new int[10][10];
+        sunkData.setNumSunk(2);
+
     }
 
     @Test
     public void testBombFire(){
         assertEquals("HIT", bomb.use(1, 1)); // Multiple hits -> return "HIT"
-        testGrid[0][1] *= -1;
-        testGrid[1][0] *= -1;
-        testGrid[1][2] *= -1;
-        assertArrayEquals(testGrid, lower.getGrid());
+        testLowerGrid[0][1] *= -1;
+        testLowerGrid[1][0] *= -1;
+        testLowerGrid[1][2] *= -1;
+        testUpperGrid[0][1] = 1;
+        testUpperGrid[1][0] = 1;
+        testUpperGrid[1][2] = 1;
+        assertArrayEquals(testLowerGrid, lower.getGrid());
+        assertArrayEquals(testUpperGrid, upper.getGrid());
     }
 
     @Test
     public void testBombTopLeftCorner(){
         assertEquals("HIT", bomb.use(0, 0)); // Multiple hits -> return "HIT"
-        testGrid[0][0] *= -1;
-        testGrid[1][0] *= -1;
-        testGrid[0][1] *= -1;
-        assertArrayEquals(testGrid, lower.getGrid());
+        testLowerGrid[0][0] *= -1;
+        testLowerGrid[1][0] *= -1;
+        testLowerGrid[0][1] *= -1;
+        assertArrayEquals(testLowerGrid, lower.getGrid());
+        //TODO: uppergrid test
     }
 
     @Test
     public void testBombBottomRightCorner(){
         assertEquals("MISS", bomb.use(9, 9)); // Should be "MISS"
-        assertArrayEquals(testGrid, lower.getGrid());
+        assertArrayEquals(testLowerGrid, lower.getGrid());
+        assertArrayEquals(testUpperGrid, upper.getGrid());
     }
 
     @Test
     public void testBombSingleHit(){
         assertEquals("HIT", bomb.use(2, 2)); // Should be "HIT DESTROYER"
-        testGrid[1][2] *= -1;
-        assertArrayEquals(testGrid, lower.getGrid());
+        testLowerGrid[1][2] *= -1;
+        assertArrayEquals(testLowerGrid, lower.getGrid());
+        //TODO: uppergrid test
     }
 
     @Test
@@ -73,25 +89,54 @@ public class BombTest {
 
     @Test
     public void testOneSink(){
-        assertEquals("MISS", bomb.use(2, 1)); //Only hits the CQ of destroyer -> "MISS" due to armor
+        lower.receiveAttack(1, 1); //hits the CQ of destroyer -> "MISS" due to armor
         assertEquals("SUNK Destroyer", bomb.use(2, 1)); // CQ of destroyer hit again -> sinks
+        //TODO: uppergrid test
     }
 
     @Test
     public void testSurrender(){
-        assertEquals("HIT", bomb.use(1, 2)); //Hits destroyer part and both ship's CQ(misses) --> "HIT"
+        lower.receiveAttack(0, 2); // hit des CQ first time
+        lower.receiveAttack(1, 1); // hit bat CQ first time
         assertEquals("SURRENDER", bomb.use(1, 2)); //Hits both ship's CQ again --> Two sinks --> Surrender
+        //TODO: uppergrid test
     }
 
     @Test
     public void testTwoSinks(){
         lower.addShip(new Minesweeper(), 5, 5, "E", false);
-        assertEquals("HIT", bomb.use(1, 2)); //Hits destroyer part and both ship's CQ(misses) --> "HIT"
+        lower.receiveAttack(0, 2); // hit des CQ first time
+        lower.receiveAttack(1, 1); // hit bat CQ first time
         String twoSinkRes = bomb.use(1, 2);
         assertTrue(twoSinkRes.equals("SUNK Battleship, SUNK Destroyer") || twoSinkRes.equals("SUNK Destroyer, SUNK Battleship")); //Hits both ship's CQ again --> Two sinks
+        //TODO: uppergrid test
     }
 
+    @Test
+    public void testNumUses(){
+        assertThrows(RuntimeException.class, () -> bomb.use(-1, 5)); // Invalid use shouldn't count as a use
+        bomb.use(0, 0);
+        assertThrows(Exception.class, () -> bomb.use(0, 0)); // only allowing one usage
+    }
 
+    @Test
+    public void testRequirements(){
+        sunkData.setNumSunk(0);
+        assertThrows(RuntimeException.class, () -> bomb.use(1, 5));
+        sunkData.setNumSunk(2);
+        bomb.use(1, 5);
+    }
+
+    @Test
+    public void testRequirementsAfterSinkingShips(){
+        sunkData.setNumSunk(0);
+
+        upper.sendAttack(0, 2);
+        upper.sendAttack(0, 2);
+        upper.sendAttack(1, 1);
+        upper.sendAttack(1, 1);
+        bomb.use(1, 5);
+    }
 
     //TODO: Test edge fires, Test Return values
 }
